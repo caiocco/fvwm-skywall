264bad9995b7c98d9f670cd5a217fd2bcf768f03
caiocco <caiocco@gmail.com>
2020-05-29 22:42:44 -0300

    Adding autodetection for US Intl and BR ABNT-2 keyboard layouts

--- fvwmroot.backup/keyb.sh
+++ fvwmroot.backup/keyb.sh
@@ -18,4 +18,15 @@ elif [ "$1" = "usnoint" ]; then
   echo "Modo: teclado US..."
   setxkbmap -model pc105 -layout us
   xmodmap ~/.Xmodmap
+elif [ "$1" = "" ]; then
+  XLAYOUT=`cat /etc/X11/xorg.conf.d/00-keyboard.conf | grep XkbLayout | awk -F' ' '{ print $3 }' | tr -d "\""`
+  if [ "$XLAYOUT" = "br" ]; then
+    echo "Modo: teclado ABNT-2..."
+    setxkbmap -model abnt2 -layout br -variant abnt2
+    xmodmap ~/.Xmodmap
+  elif [ "$XLAYOUT" = "us_intl" ]; then
+    echo "Modo: teclado US Internacional..."
+    setxkbmap -model pc105 -layout us -variant intl
+    xmodmap ~/.Xmodmap
+  fi
 fi
