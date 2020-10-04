a8b840c316c00fd1ba7b5b9e402d495108fe603b
caiocco <caiocco@gmail.com>
2020-10-04 18:43:50 -0300

    Now wmctrl grabs the selected window using the id (precision improvement); now any occurrence of "N/A" will be replaced with the hostname

--- fvwmroot.backup/window_finder.sh
+++ fvwmroot.backup/window_finder.sh
@@ -3,12 +3,16 @@
 #
 # Colaborou Phantom X
 #
+host=`hostname`
 random=`awk -v min=100009 -v max=999999 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
-wmctrl -l | grep -v "Localizador de Janelas" | cat -n > /tmp/${random}_fvwm_window_list.txt
+wmctrl -l | grep -v "Localizador de Janelas" | sed 's/            N\/A/'$host'/g' | cat -n > /tmp/${random}_fvwm_window_list.txt
 selection=`cat /tmp/${random}_fvwm_window_list.txt | fzf -e`
 id=`echo $selection | cut -f 2 -d " "`
-window_name=`wmctrl -l 2> /dev/null | grep $id | sed 's/  /_/g;s/ /_/g' | cut -d_ -f4,4- | sed 's/_/ /g'`
-wmctrl -a ${window_name}
+# Não é necessário filtrar pelo nome a partir do ID, é mais eficiente usar o ID diretamente, comentando código abaixo...
+#window_name=`wmctrl -l 2> /dev/null | grep $id | sed 's/  /_/g;s/ /_/g' | cut -d_ -f4,4- | sed 's/_/ /g'`
+#window_name_fix=`echo $window_name | sed 's/^N\/A //g'`
+#wmctrl -a ${window_name_fix}
+wmctrl -i -a $id
 
 trap cleanup EXIT
 cleanup() {
